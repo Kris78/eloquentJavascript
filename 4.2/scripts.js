7787be09c72cd70333e23c827a2111258db06bfe
Christophe Hamet <c.hamet@betclicgroup.com>
2017-11-23 17:47:03 +0100

    exo 4.2

---
+++ 4.2/scripts.js
@@ -0,0 +1,31 @@
+function reverseArray(array) {
+
+	var output = [];
+	for (var i = array.length; i > 0; i--) {
+		output.push(array[i - 1]);
+	}
+	return output;
+}
+
+
+
+function reverseArrayInPlace(array) {
+
+	for (var i = 0; i < Math.floor(array.length / 2); i++) {
+		var old = array[i];
+		array[i] = array[array.length - 1 - i];
+		array[array.length - 1 - i] = old;
+	}
+
+	return array;
+
+}
+
+
+//console.log(reverseArray(["A", "B", "C"]));
+// → ["C", "B", "A"];
+
+var arrayValue = [1, 2, 3, 4, 5];
+reverseArrayInPlace(arrayValue);
+console.log(arrayValue);
+// → [5, 4, 3, 2, 1]
